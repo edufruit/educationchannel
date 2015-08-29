5eaad0343e4e7ea6157e09ed09a7e1efc0c75274
edufruit <edufruit@gmail.com>
2015-08-29 22:00:50 +0530

    panels

--- www/js/swipe_function.js
+++ www/js/swipe_function.js
@@ -1,10 +1,10 @@
 
 
 
-$( document ).on( "pageinit", "#demo-page", function() {
+	$( document ).on( "pageinit", "#pageone", function() {
 	
 		
-		$( document ).on( "swipeleft swiperight", "#demo-page", function( e ) {
+		$( document ).on( "swipeleft swiperight", "#pageone", function( e ) {
 			// We check if there is no open panel on the page because otherwise
 			// a swipe to close the left panel would also open the right panel (and v.v.).
 			// We do this by checking the data that the framework stores on the page element (panel: open).
@@ -16,5 +16,53 @@ $( document ).on( "pageinit", "#demo-page", function() {
 				}
 			}
 		});
+		 
+ 
+ 
+ 
+		//$('.headerChild').load('top-menu.html')
+	 
+		//$( "[data-role='navbar']" ).navbar();
+	 
+	 
+		$( "[data-role='header'], [data-role='footer']" ).toolbar();
+			  
+			  
+		$(document).on('pagebeforecreate', function () {
 		
+			 
+			var panel = $('#left-panel')[0].outerHTML;;	
+		
+			$.mobile.pageContainer.prepend(panel);
+			
+			$("#left-panel").panel().enhanceWithin();
+			
+			//$('#pageone').find('[data-role=panel]').panel('open');
+			
+		});
+		
+		
+
 	});
+	
+		
+	/*	
+	
+	$( document ).on( "pagecontainershow", function() {
+		// Each of the four pages in this demo has a data-title attribute
+		// which value is equal to the text of the nav button
+		// For example, on first page: <div data-role="page" data-title="Info">
+		var current = $( ".ui-page-active" ).jqmData( "title" );
+		// Change the heading
+		$( "[data-role='header'] h1" ).text( current );
+		// Remove active class from nav buttons
+		$( "[data-role='navbar'] a.ui-btn-active" ).removeClass( "ui-btn-active" );
+		// Add active class to current nav button
+		$( "[data-role='navbar'] a" ).each(function() {
+			if ( $( this ).text() === current ) {
+				$( this ).addClass( "ui-btn-active" );
+			}
+		});
+	});
+	
+	*/
