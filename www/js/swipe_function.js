9e4c8cd6b3add2c6b18ff293729af0a7bf7d034b
edufruit <edufruit@gmail.com>
2015-08-30 12:17:51 +0530

    asss

--- www/js/swipe_function.js
+++ www/js/swipe_function.js
@@ -1,10 +1,12 @@
 
 
 
-	$( document ).on( "pageinit", "#pageone", function() {
+	//$( document ).on( "pageinit", "#pageone", function() {
+	$( document ).on( "pageinit", function() {
 	
 		
-		$( document ).on( "swipeleft swiperight", "#pageone", function( e ) {
+		//$( document ).on( "swipeleft swiperight", "#pageone", function( e ) {
+		$( document ).on( "swipeleft swiperight", function( e ) {
 			// We check if there is no open panel on the page because otherwise
 			// a swipe to close the left panel would also open the right panel (and v.v.).
 			// We do this by checking the data that the framework stores on the page element (panel: open).
